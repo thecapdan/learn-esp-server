69a72fdeb982fa9b7146dc127f393324c774d397
thecapdan <zdano80@gmail.com>
2021-05-03 20:57:40 +0100

    pass db credentials as npm args

--- src/server.js
+++ src/server.js
@@ -2,16 +2,17 @@ import express from "express";
 import { routes } from "./routes";
 import mongoose from "mongoose";
 
+let user = process.env.npm_config_user || "readonly";
+let password = process.env.npm_config_password || "readonly";
+
 const app = express();
 app.use(express.json());
 
-// use cd21 for write
-const dbURI =
-  "mongodb+srv://readonly:readonly@cluster0.qtarf.mongodb.net/learn-esp?retryWrites=true&w=majority";
+const dbURI = `mongodb+srv://${user}:${password}@cluster0.qtarf.mongodb.net/learn-esp?retryWrites=true&w=majority`;
 mongoose
   .connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then((result) => {
-    console.log("Connected to learn-esp db");
+    console.log(`Connected to learn-esp db with ${user}`);
     app.listen(8080, () => {
       console.log("Server is listening on port 8080");
     });
